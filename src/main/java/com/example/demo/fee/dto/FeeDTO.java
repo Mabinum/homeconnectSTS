6f59dec078d7594823f798d614092882109194dd
pokm2360 <por0632@naver.com>
2024-06-27 10:36:24 +0900

    ㅇㅇ

--- src/main/java/com/example/demo/fee/dto/FeeDTO.java
+++ src/main/java/com/example/demo/fee/dto/FeeDTO.java
@@ -1,5 +1,25 @@
 package com.example.demo.fee.dto;
 
-public class FeeDTO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FeeDTO {
+	
+	int no; // id
+	
+	int water; // 수도세
+	
+	int electric; // 전기세
+	
+	int maintenance; // 관리비 
 }
