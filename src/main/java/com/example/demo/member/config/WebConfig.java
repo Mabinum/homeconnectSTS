18df9ace60a821c020119fcf47d2057fe046cab5
Mabin <mabinum9528@naver.com>
2024-07-11 15:28:18 +0900

    i

--- src/main/java/com/example/demo/member/config/WebConfig.java
+++ src/main/java/com/example/demo/member/config/WebConfig.java
@@ -24,7 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3001")
                 .allowedOrigins("http://localhost:3002")
+                .allowedOrigins("http://localhost:3003")
+                .allowedOrigins("http://localhost:3004")
+                .allowedOrigins("http://localhost:3005")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
