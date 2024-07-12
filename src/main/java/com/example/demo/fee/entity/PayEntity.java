227c2144b4fed861203201c7c9357889e042710a
pokm2360 <por0632@naver.com>
2024-07-12 12:23:35 +0900

    fee수정

--- src/main/java/com/example/demo/fee/entity/PayEntity.java
+++ src/main/java/com/example/demo/fee/entity/PayEntity.java
@@ -1,33 +1,33 @@
-//package com.example.demo.fee.entity;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import lombok.ToString;
-//
-//@Entity
-//@Table(name = "pay")
-//@Getter
-//@Setter
-//@ToString
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class PayEntity {
-//
-//	@Id
-//	@Column(length = 100)
-//	String merchant_uid;
-//	
-//	@Column(length = 100)
-//	String imp_uid;
-//	
-//	@Column
-//	int amount;
-//}
+package com.example.demo.fee.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "pay")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PayEntity {
+
+	@Id
+	@Column(length = 100)
+	String merchant_uid;
+	
+	@Column(length = 100)
+	String imp_uid;
+	
+	@Column
+	int amount;
+}
--- src/main/java/com/example/demo/fee/service/PayService.java
+++ src/main/java/com/example/demo/fee/service/PayService.java
@@ -1,31 +1,31 @@
-//package com.example.demo.fee.service;
-//
-//import java.util.List;
-//
-//import com.example.demo.fee.dto.PayDTO;
-//import com.example.demo.fee.entity.PayEntity;
-//
-//public interface PayService {
-//
-//	String register(PayDTO dto);
-//	List<PayDTO> getList();
-//	PayDTO read(String merchant_uid);
-//	
-//	default PayEntity dtoToEntity(PayDTO dto) {
-//		PayEntity entity = PayEntity.builder()
-//				.imp_uid(dto.getImp_uid())
-//				.merchant_uid(dto.getMerchant_uid())
-//				.amount(dto.getAmount())
-//				.build();
-//		return entity;
-//	}
-//	
-//	default PayDTO entityToDto(PayEntity entity) {
-//		PayDTO dto = PayDTO.builder()
-//				.imp_uid(entity.getImp_uid())
-//				.merchant_uid(entity.getMerchant_uid())
-//				.amount(entity.getAmount())
-//				.build();
-//		return dto;
-//	}
-//}
+package com.example.demo.fee.service;
+
+import java.util.List;
+
+import com.example.demo.fee.dto.PayDTO;
+import com.example.demo.fee.entity.PayEntity;
+
+public interface PayService {
+
+	String register(PayDTO dto);
+	List<PayDTO> getList();
+	PayDTO read(String merchant_uid);
+	
+	default PayEntity dtoToEntity(PayDTO dto) {
+		PayEntity entity = PayEntity.builder()
+				.imp_uid(dto.getImp_uid())
+				.merchant_uid(dto.getMerchant_uid())
+				.amount(dto.getAmount())
+				.build();
+		return entity;
+	}
+	
+	default PayDTO entityToDto(PayEntity entity) {
+		PayDTO dto = PayDTO.builder()
+				.imp_uid(entity.getImp_uid())
+				.merchant_uid(entity.getMerchant_uid())
+				.amount(entity.getAmount())
+				.build();
+		return dto;
+	}
+}
