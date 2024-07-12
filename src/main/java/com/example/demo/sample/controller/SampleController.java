14e8dcff359265a9f775409d8a257806af4eb8c8
Mabin <mabinum9528@naver.com>
2024-07-12 10:45:21 +0900

    12

--- src/main/java/com/example/demo/sample/controller/SampleController.java
+++ src/main/java/com/example/demo/sample/controller/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SampleController {
 	
-	@GetMapping("/")
+	@GetMapping("/ex1")
 	public String ex1() {
 		return "ex1";
 	}
