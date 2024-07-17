997849f7a41be89513ea5702661eb067de41bd9b
Mabin <mabinum9528@naver.com>
2024-07-17 10:37:42 +0900

    1

--- src/main/java/com/example/demo/member/repository/MemberRepository.java
+++ src/main/java/com/example/demo/member/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.example.demo.member.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.member.entity.Member;
 
@@ -13,5 +16,16 @@ public interface MemberRepository extends JpaRepository<Member,String >{
 	Optional<Member> findByUserId(String userId);
 	
 	void deleteByUserId(String userId);
+	
 
+    @Modifying
+    @Query("UPDATE Member m SET m.community_no = :communityNo WHERE m.user_id = :userId")
+    void updateCommunityNoByUserId(@Param("communityNo") String communityNo, @Param("userId") String userId);
+    
+//    public void updateCommunityNoForAdmin() {
+//        String userId = "admin";
+//        String newCommunityNo = "1,2,3,4,5";
+//        memberRepository.updateCommunityNoByUserId(newCommunityNo, userId);
+//    }
+	
 }
